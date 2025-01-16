cbf638ac8f6884e039de81e62378e62fcb5dfc35
Yoakim <ykmpersson@gmail.com>
2025-01-16 20:50:33 +0800

    1. Added basic api stuff

---
+++ server/api.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func dataFn(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
--- server/main.go
+++ server/main.go
@@ -5,22 +5,38 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("hello")
+
 	r := gin.Default()
+
+	// r.SetTrustedProxies([]string{"mini-farm-tracker.io"})
+	// r.ForwardedByClientIP = true
+
+	api := r.Group("/api")
+	{
+		api.GET("/data", dataFn)
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	// if err := r.Run(":3000"); err != nil {
-	// 	log.Fatalf("Failed to run server: %v", err)
-	// }
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
 
-	log.Fatal(autotls.Run(r, "mini-farm-tracker.io"))
+	log.Println("Starting server on port 3000...")
+
+	// log.Fatal(autotls.Run(r, "mini-farm-tracker.io"))
+	log.Fatal(r.Run(":3000"))
+
+	log.Println("Server has started successfully.")
 }
