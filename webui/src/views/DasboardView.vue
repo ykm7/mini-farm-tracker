83aac7df68ab1ae93f844542ce591f335e7744ca
Yoakim <ykmpersson@gmail.com>
2025-01-27 14:25:46 +0800

    1. Corrected screen size issues

--- webui/src/views/DasboardView.vue
+++ webui/src/views/DasboardView.vue
@@ -19,6 +19,7 @@ import DashboardHeader from '../components/DashboardHeader.vue'
 .main-content {
   padding: 2rem;
   min-height: 100vh;
+  min-width: 100vw;
   display: flex;
   flex-direction: column;
 }
