fe99718e372a1965516401128e1ad7874756511a
Yoakim <ykmpersson@gmail.com>
2025-01-28 18:03:39 +0800

    1. Remove "ping" related UI and logic. Have moved past needing it

--- webui/src/components/DashboardHeader.vue
+++ webui/src/components/DashboardHeader.vue
@@ -11,34 +11,13 @@
         <li><router-link to="/sensor">Sensors</router-link></li>
       </ul>
     </nav>
-    <div class="ping-test">
-      <button class="button" @click="pingServerFn" title="ping server">ping server</button>
-      <a>{{ pingMsg }}</a>
-    </div>
     <div class="user-info">
-      <span class="username">John Doe</span>
+      <span class="username">Default User (Reader)</span>
     </div>
   </header>
 </template>
 
-<script setup lang="ts">
-import axios from 'axios'
-import { ref } from 'vue'
-
-const BASE_URL: string = import.meta.env.VITE_BASE_URL
-const pingMsg = ref('')
-
-const pingServerFn = async () => {
-  console.log('Attempting to ping server')
-  try {
-    await axios.get(`${BASE_URL}/ping`)
-    pingMsg.value = 'success'
-  } catch (e) {
-    console.warn(e)
-    pingMsg.value = 'failure'
-  }
-}
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .dashboard-header {
