0647e5a04bfc832e6cf1a6a49e612d38f054725e
Yoakim <ykmpersson@gmail.com>
2025-02-19 13:44:19 +0800

    1. Added a UX flicker prevention timeout to stop the loading icon from    displaying this below a ms threshold

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <Suspense>
+  <Suspense @pending="onPending" @resolve="onResolve">
     <template #default>
       <AsyncApp />
     </template>
     <template #fallback>
-      <LoadingIcon />
+      <LoadingIcon v-if="showLoading" />
     </template>
   </Suspense>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
-import LoadingIcon from "@/components/LoadingIcon.vue"
+  import LoadingIcon from "@/components/LoadingIcon.vue"
+  import { defineAsyncComponent, onMounted, ref } from "vue"
 
-const AsyncApp = defineAsyncComponent(() => import('./AsyncApp.vue'))
+  const AsyncApp = defineAsyncComponent(() => import("./AsyncApp.vue"))
+
+  const showLoading = ref(false)
+  let loadingTimeout: number | null = null
+
+  const onPending = () => {
+    loadingTimeout = setTimeout(() => {
+      showLoading.value = true
+      // diplay displaying a loading indicator. If we don't we "flicker" the loading icon which is poor UX. 
+    }, 200)
+  }
+
+  const onResolve = () => {
+    if (loadingTimeout) {
+      clearTimeout(loadingTimeout)
+    }
+    showLoading.value = false
+  }
+
+  onMounted(() => {
+    // Clean up the timeout if the component is unmounted
+    return () => {
+      if (loadingTimeout) {
+        clearTimeout(loadingTimeout)
+      }
+    }
+  })
 </script>
