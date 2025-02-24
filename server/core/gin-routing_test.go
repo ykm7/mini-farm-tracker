2666fddb4039b00cea044527ec39e7a0d0889598
Yoakim <ykmpersson@gmail.com>
2025-02-24 12:07:31 +0800

    1. Added both rate limiter (across IP addresses) 2. Added concurrency rate limiter.
    Both of these are primarily focused Gins NoRoutes (which are all
    unassigned routes)

--- server/core/gin-routing_test.go
+++ server/core/gin-routing_test.go
@@ -1,12 +1,16 @@
 package core
 
 import (
-	"context"
 	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,15 +23,7 @@ func mapToJSONString(m any) (string, error) {
 }
 
 func TestSetupRouter(t *testing.T) {
-	db, deferFn := MockSetupMongo(context.TODO())
-	defer deferFn()
-
-	mongoDb := &MongoDatabaseImpl{Db: db}
-
-	server := &Server{
-		MongoDb: mongoDb,
-		Sensors: NewSyncCache[string, Sensor](),
-	}
+	server := &Server{}
 
 	router := SetupRouter(server)
 
@@ -35,9 +31,100 @@ func TestSetupRouter(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, http.StatusOK, w.Code)
 
 	expectedJson, err := mapToJSONString(map[string]string{"message": "pong"})
 	assert.ErrorIs(t, err, nil)
 	assert.Equal(t, expectedJson, w.Body.String())
+}
+
+func TestNoRouteSingleRequest(t *testing.T) {
+	server := &Server{}
+
+	router := SetupRouter(server)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping_random", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+}
+
+func TestNoRouteTriggerRateLimiter(t *testing.T) {
+	server := &Server{}
+
+	router := SetupRouter(server)
+
+	req, _ := http.NewRequest("GET", "/ping_random", nil)
+	for range NO_ROUTE_BURST {
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusNotFound, w.Code)
+	}
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusTooManyRequests, w.Code)
+}
+
+/*
+*
+Overall this was more to test functionality of the concurrency rather than be all that useful.
+By default the NoRoute finishes so quickly that it cannot be used to without arbitrarily adding sleep
+*/
+func TestNoRouteTriggerConcurrency(t *testing.T) {
+	server := &Server{}
+
+	router := SetupRouter(server)
+
+	// Idea where was that the semaphore within the `ConcurrencyLimiter` might not have time to setup the sem.
+	tempSlowNoRouteFn := func(c *gin.Context) {
+		time.Sleep(1 * time.Second)
+		c.Status(http.StatusNotFound)
+	}
+
+	noRoutes := []gin.HandlerFunc{}
+	noRoutes = append(noRoutes, noRoute()...)
+	noRoutes = append(noRoutes, tempSlowNoRouteFn)
+
+	router.NoRoute(noRoutes...)
+
+	additionalRequests := 7
+
+	returnStatus := make(chan int, NO_ROUTE_CONCURRENCY_LIMIT+additionalRequests)
+
+	var wg sync.WaitGroup
+	for i := range NO_ROUTE_CONCURRENCY_LIMIT + additionalRequests {
+		wg.Add(1)
+		go func(v int) {
+			defer wg.Done()
+
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/ping_random", nil)
+			req.RemoteAddr = fmt.Sprintf("192.168.1.%d:1234", v+1)
+
+			router.ServeHTTP(w, req)
+
+			returnStatus <- w.Code
+		}(i)
+	}
+
+	wg.Wait()
+	close(returnStatus)
+
+	statuses := make(map[int]int)
+
+	for status := range returnStatus {
+		statuses[status] += 1
+	}
+
+	/**
+	At this point should have:
+	* (NO_ROUTE_CONCURRENCY_LIMIT) number of 404 return codes.
+	* (additionalRequests) number of 503
+	*/
+	assert.Equal(t, statuses[http.StatusNotFound], NO_ROUTE_CONCURRENCY_LIMIT)
+	assert.Equal(t, statuses[http.StatusServiceUnavailable], additionalRequests)
 }
